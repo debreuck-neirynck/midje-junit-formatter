8a05e42f44fc5e4232e89b0a09a1fee7cc1e21be
Wout Neirynck <wout.neirynck@d-n.be>
2020-02-10 12:43:41 +0100

    Enabled gitlab build

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject midje-junit-formatter "0.1.0-SNAPSHOT"
+(defproject dn/midje-junit-formatter "0.1.1-SNAPSHOT"
   :description "Formats the midje output to a JUnit-style XML for CI/CD purposes"
   :url "https://www.d-n.be"
   :license {:name "Eclipse Public License"
@@ -6,4 +6,10 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [midje "1.9.9"]
                  [clojure.java-time "0.3.2"]]
-  :profiles {:dev {:plugins [[lein-midje "3.2.1"]]}})
+  :profiles {:dev {:plugins [[lein-midje "3.2.1"]]}}
+  :deploy-repositories [["snapshots" {:url "https://nexus.d-n.be/repository/dn-snapshots"
+                                      :username [:gpg :env/nexus_user]
+                                      :password [:gpg :env/nexus_pass]}]
+                        ["releases"  {:url "https://nexus.d-n.be/repository/dn-releases"
+                                      :username [:gpg :env/nexus_user]
+                                      :password [:gpg :env/nexus_pass]}]])
